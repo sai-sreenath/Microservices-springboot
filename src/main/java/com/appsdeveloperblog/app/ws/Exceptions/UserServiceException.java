bc03e55bedee7d5ee949f24e2e7f46a41156f314
sai-sreenath <ppvsaisreenath565@gmail.com>
2021-04-20 19:53:02 -0400

    my first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/Exceptions/UserServiceException.java
@@ -0,0 +1,10 @@
+package com.appsdeveloperblog.app.ws.Exceptions;
+
+public class UserServiceException extends RuntimeException{
+
+    public UserServiceException(String message)
+    {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,104 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import com.appsdeveloperblog.app.ws.Exceptions.UserServiceException;
+import com.appsdeveloperblog.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users") //http://localhost:8080/users
+public class UserController {
+
+    Map<String, UserRest> users;
+
+    //required should be used with request parameters strings and not strings
+    @GetMapping
+    public String getUsers(@RequestParam(value= "page") int page,
+                           @RequestParam(value="limit", defaultValue = "50") int limit,
+                           @RequestParam(value="sort", required = false) String sort){
+        return "get users was called with page = " + page + " and limit = " + limit + "sort = " + sort;
+    }
+
+    @GetMapping(path = "/{userId}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> getUser(@PathVariable String userId)
+    {
+        if(true)
+            throw new UserServiceException("A user service exception is thrown");
+
+        if(users.containsKey(userId))
+        {
+            return new ResponseEntity<UserRest>(users.get(userId), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<UserRest>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            consumes={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> createUser( @Valid @RequestBody UserDetailsRequestModel userDetails)
+    {
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+
+        String userId = UUID.randomUUID().toString();
+        returnValue.setUserId(userId);
+
+        if(users == null)
+            users = new HashMap<>();
+        users.put(userId, returnValue);
+
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{userId}",
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            consumes={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails)
+    {
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+
+        users.put(userId, storedUserDetails);
+
+        return storedUserDetails;
+    }
+
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity deleteUser(@PathVariable String id)
+    {
+        users.remove(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
