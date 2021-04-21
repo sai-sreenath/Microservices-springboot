90a47840d5b14bcad4bac219520bc62bc7102f11
sai-sreenath <ppvsaisreenath565@gmail.com>
2021-04-21 12:28:03 -0400

    User Microservice setup with rest controller, discovery client

---
+++ PhotoAppApiUsers/src/test/java/com/appsdeveloperblog/photoapi/api/users/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapi.api.users.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
